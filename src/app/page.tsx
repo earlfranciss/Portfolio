a1fb6f0b0aec97e91795cffe5a6b6c47db82171a
earlfranciss <165235568+earlfranciss@users.noreply.github.com>
2025-12-22 08:47:55 +0800

    Add: Snowflake effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,33 @@
+"use client"
+
 import About from "./about/page";
 import Projects from "./projects/page";
 import Experience from "./experience/page";
 import Github from "./github/page";
 import Contact from "./contact/page";
+import Snowfall from "react-snowfall";
 
 export default function Home() {
   return (
     <section className="space-y-4">
+      <Snowfall
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+          zIndex: 100,
+          pointerEvents: "none",
+        }}
+        color="#daeeffff"
+        snowflakeCount={400}
+        speed={[2, 4]}
+        wind={[1, 4]}
+        radius={[0.5, 3.5]}
+      />
       <About />
-      <Projects/>
+      <Projects />
       <Experience />
       <Github />
       <Contact />
--- src/app/projectDetails/[id]/layout.tsx
+++ src/app/projectDetails/[id]/layout.tsx
@@ -1,3 +1,7 @@
+"use client"
+
+import Snowfall from "react-snowfall";
+
 // app/projectDetails/[id]/layout.tsx
 export default function ProjectDetailsLayout({
   children,
@@ -7,6 +11,22 @@ export default function ProjectDetailsLayout({
   return (
     <div className="min-h-screen flex justify-center px-4 sm:px-6 lg:px-8">
       <main className="w-full max-w-4xl px-4 mx-auto">
+        <Snowfall
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            width: "100vw",
+            height: "100vh",
+            zIndex: 100,
+            pointerEvents: "none",
+          }}
+          color="#daeeffff"
+          snowflakeCount={400}
+          speed={[2, 4]}
+          wind={[1, 4]}
+          radius={[0.5, 3.5]}
+        />
         {children}
       </main>
     </div>
